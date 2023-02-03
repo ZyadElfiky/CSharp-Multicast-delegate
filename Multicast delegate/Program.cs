e6a0383406587ed906eca3d32122d3398524e1e8
zyad <https://github.com/ZyadElfiky>
2023-02-03 21:43:45 +0200

    Add project files.

---
+++ Multicast delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Multicast_delegate
+{
+    public delegate void multicastDelegate(double width, double hight);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var helper = new RectangleHelper();
+            multicastDelegate rect;
+            
+            // subscribing
+            rect= helper.calcArea;
+            rect += helper.calcPerimeter;
+
+            rect(5, 5);
+
+
+            Console.WriteLine("After unsubscribing rect.calcArea : \n");
+            rect -= helper.calcArea;
+            
+            rect(5, 5);
+
+
+        }
+    }
+    public class RectangleHelper
+    {
+        public void calcArea(double width,double hight)
+        {
+            Console.WriteLine($"Area = {width * hight} \n" );
+        }
+        public void calcPerimeter(double width,double hight)
+        {
+            Console.WriteLine($"Perimeter = {width * hight} \n" );
+        }
+
+    }
+}
